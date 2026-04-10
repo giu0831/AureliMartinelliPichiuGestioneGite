11b48796606d68456e7a283cef74e7bfe2f97f8b
Pichiu Florin <pichiu.florin@franchettisalviani.net>
2026-04-10 11:54:39 +0200

    Classi partecipazione, gita e studente

--- AureliMartinelliPichiuGestioneGite/src/aurelimartinellipichiugestionegite/Gita.java
+++ AureliMartinelliPichiuGestioneGite/src/aurelimartinellipichiugestionegite/Gita.java
@@ -9,5 +9,26 @@ package aurelimartinellipichiugestionegite;
  * @author aureli.giulia
  */
 public class Gita {
+    private String destinazione;
+    private int durata;
+    private int prezzo;
+
+    public Gita(String destinazione, int durata, int prezzo) {
+        this.destinazione = destinazione;
+        this.durata = durata;
+        this.prezzo = prezzo;
+    }
+
+    public String getDestinazione() {
+        return destinazione;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
     
 }
--- AureliMartinelliPichiuGestioneGite/src/aurelimartinellipichiugestionegite/Studente.java
+++ AureliMartinelliPichiuGestioneGite/src/aurelimartinellipichiugestionegite/Studente.java
@@ -9,5 +9,33 @@ package aurelimartinellipichiugestionegite;
  * @author aureli.giulia
  */
 public class Studente {
+    private int matricola;
+    private String nome;
+    private String cognome;
+    private Classe classe;
+    //private int IDClasse;
+
+    public Studente(int matricola, String nome, String cognome, Classe classe) {
+        this.matricola = matricola;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.classe = classe;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public Classe getClasse() {
+        return classe;
+    }
     
 }
